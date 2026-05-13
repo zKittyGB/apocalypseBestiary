8356aa0b7c33d09449e3aeedde4858f18526b6b5
zKittyGB <a-hebert@hotmail.fr>
2026-05-13 15:24:34 +0200

    Refactor database connection to use external config
    Fix d'une faille explicite pour la DB

--- config/database.php
+++ config/database.php
@@ -7,21 +7,8 @@ class Database
 	public static function getInstance(): PDO
 	{
 		if (self::$instance === null) {
-			try {
-				$host = "zkittyabangbang.mysql.db";
-				$dbname = "zkittyabangbang"; 
-				$username = "zkittyabangbang"; 
-				$password = "Patate2TesMorts";
-				$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
-
-				self::$instance = new PDO($dsn, $username, $password, [
-					PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Affiche les erreurs SQL
-					PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Retourne des tableaux associatifs
-					PDO::ATTR_EMULATE_PREPARES => false, // Améliore la sécurité des requêtes préparées
-				]);
-			} catch (PDOException $e) {
-				die("Erreur de connexion à la base de données : " . $e->getMessage());
-			}
+			require __DIR__ . '/../../../../include/db.php';
+			self::$instance = $pdo;
 		}
 
 		return self::$instance;
