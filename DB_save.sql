38f9e4d500cae8d3e4a945fe54e6e102856bc934
zKittyGB <a-hebert@hotmail.fr>
2026-05-13 13:21:07 +0200

    Update DB_save.sql
    Hide useless DB details

--- DB_save.sql
+++ DB_save.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Hôte : zkittyabangbang.mysql.db
--- Généré le : lun. 17 mars 2025 à 12:56
--- Version du serveur : 8.0.40-31
--- Version de PHP : 7.4.33
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
